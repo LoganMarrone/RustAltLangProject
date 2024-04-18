522e5cad8dbc9ddf1abef6f0b8b900144fcfe0c7
Logan Marrone <lmarrone2021@fau.edu>
2024-04-18 16:53:08 -0400

    Read the file in Rust using std::fs::file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::fs::File;
+
 fn main (){
-    println!("Hello World!");
+    //establish mutabile var file for the file that checks if the file is Ok and if there runs an arror
+    let mut file = match File::open("./cells.csv"){
+        Ok(file) => file,
+        Err(ohnoerror) => {
+            println!("There was an error reading the file: {}", ohnoerror);
+            return;
+        }
+    };
+
 }
